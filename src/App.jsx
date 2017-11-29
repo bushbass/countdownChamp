9d599b5b2b74be53cfbcfc674ca525fd0f5cca1b
Alex Nielsen <alex.nielsen@gmail.com>
2017-11-29 17:12:26 -0500

    finished first part of learn about state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: "December 25, 2017",
+      newDeadline: '',
+    }
+  }
+
+changeDeadline() {
+  this.setState({deadline: this.state.newDeadline});
+}
+
   render() {
     return (
     <div className='App'>
-      <div className='App-title'>Countdown to December 25, 2017</div>
+      <div className='App-title'>Countdown to {this.state.deadline}</div>
       <div>
         <div className='Clock-days'>14 days</div>
         <div className='Clock-hours'>30 hours</div>
@@ -13,8 +26,8 @@ class App extends Component {
         <div className='Clock-seconds'>20 seconds</div>
       </div>
       <div>
-        <input placeholder='new date' type="text"/>
-        <button>submit</button>
+        <input onChange={event=> this.setState({newDeadline: event.target.value})} placeholder='new date' type="text"/>
+        <button onClick= {()=> this.changeDeadline()}>submit</button>
       </div>
       
     </div>
