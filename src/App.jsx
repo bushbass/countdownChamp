bed4ae7822e50312764465e9eb4ecf3958cdf10c
Alex <alex.nielsen@gmail.com>
2017-11-30 21:42:42 -0500

    finished all of first project but not extra credit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import React, {Component} from 'react';
 import './App.css';
-import Clock from './Clock'
+import Clock from './Clock';
+import {Form, FormControl, Button} from 'react-bootstrap';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       deadline: "December 25, 2017",
-      newDeadline: ''
+      newDeadline: '',
     }
   }
 
-  
   changeDeadline() {
     this.setState({deadline: this.state.newDeadline});
   }
-
-
 
   render() {
     return (
     <div className='App'>
       <div className='App-title'>Countdown to {this.state.deadline}</div>
-     <Clock deadline = {this.state.deadline} />
-      <div>
-        <input onChange={event=> this.setState({newDeadline: event.target.value})} placeholder='new date' type="text"/>
-        <button onClick= {()=> this.changeDeadline()}>submit</button>
-      </div>
-      
+      <Clock deadline = {this.state.deadline} />
+      <Form inline>
+        <FormControl 
+          className="Deadline-input" 
+          onChange={event=> this.setState({newDeadline: event.target.value})} 
+          placeholder='new date' type="text"/>
+        <Button 
+          onClick= {()=> this.changeDeadline()}>submit
+        </Button>
+      </Form>
     </div>
     )
   }
